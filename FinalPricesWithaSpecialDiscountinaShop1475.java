84dfd51563d957d3994af7ad55b106a04bd2118c
JDOV7 <42183896+JDOV7@users.noreply.github.com>
2020-12-03 20:33:19 -0600

    Update FinalPricesWithaSpecialDiscountinaShop1475.java

--- FinalPricesWithaSpecialDiscountinaShop1475.java
+++ FinalPricesWithaSpecialDiscountinaShop1475.java
@@ -5,16 +5,11 @@ public class FinalPricesWithaSpecialDiscountinaShop1475 {
     public void FinalPricesWithaSpecialDiscountinaShop1475() {
         int[] prices = {10, 1, 1, 6};
         for (int i = 0; i < prices.length; i++) {
-            boolean bControl = false;
             for (int j = i + 1; j < prices.length; j++) {
                 if (prices[i] >= prices[j]) {
                     prices[i] = prices[i] - prices[j];
-                    bControl = true;
                     break;
                 }
-            }
-            if (bControl == false) {
-                prices[i] = prices[i];
             }
         }
         for (int i = 0; i < prices.length; i++) {
